50886991c7c58c0e49cd79b52644880f6cbeec96
Louis Goessling <louis@goessling.com>
2013-03-28 09:06:52 -0500

    dqsty fsdjnpstf

--- FRC3699/src/org/robot3699/SmartDashboardUpdater.java
+++ FRC3699/src/org/robot3699/SmartDashboardUpdater.java
@@ -14,5 +14,10 @@ public class SmartDashboardUpdater {
     public void updateSmartDashboard(Team3699Robot robo){
         SmartDashboard.putBoolean("Intake On", robo.intake.state==1);
         SmartDashboard.putBoolean("Elevator Moving", robo.elevator.state==1);
+        SmartDashboard.putBoolean("Arm Moving", robo.arm.state==1||robo.arm.state==2);
+        SmartDashboard.putNumber("Num. Discs", robo.elevator.numDiscs);
+        SmartDashboard.putNumber("Distance To Target", robo.server.getNumber("NewDistance"));
+        SmartDashboard.putBoolean("Target Locked", robo.server.getBoolean("locked"));
+        SmartDashboard.putBoolean("Connected To RoboRealm", robo.server.isConnected());
     }
 }
